747f4d124e7b392e70d21a0d461c55fcbaa4e0e3
fabian-pandas <fabianstoehr@gmx.de>
2019-06-22 16:51:59 +0200

    added from jupyter

--- lintut/tutorial.py
+++ lintut/tutorial.py
@@ -8,5 +8,8 @@ This will be updated soon.
 ## Import
 import numpy as np
 
+a = np.array([1,2,3])
+b = np.array([(1+5j,2j,3j), (4j,5j,6j)])
+c = np.array([[(1.5,2,3), (4,5,6)], [(3,2,1), (4,5,6)]])
 
-
+print(a)
